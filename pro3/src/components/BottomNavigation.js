0b5d574a0b61b1b8d748ab57af039adbda9223ea
JulieAdele <juliesjaavaag@outlook.com>
2018-10-03 12:20:39 +0200

    Kommentert og ryddet

--- pro3/src/components/BottomNavigation.js
+++ pro3/src/components/BottomNavigation.js
@@ -1,47 +1,51 @@
 import React, {Component} from 'react';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
-import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
 //Disse skal erstattes med de faktiske sidene som skal brukes:
 import HomePageJulie from './HomePageJulie';
 import TodoPageJulie from './TodoPageJulie';
 import AppointmentsPageJulie from './AppointmentsPageJulie';
 
+//Alt med navnet Julie i seg skal endres!
 
 const HomePageRoute = () => <HomePageJulie></HomePageJulie>;
 const TodoPageRoute = () => <TodoPageJulie></TodoPageJulie>;
 const AppointmentsPageRoute = () => <AppointmentsPageJulie></AppointmentsPageJulie>;
 
-
+/*A material-design themed tab bar on the bottom of the screen that lets you switch between different routes.
+  https://reactnavigation.org/docs/en/material-bottom-tab-navigator.html */
 export default createMaterialBottomTabNavigator ({
-  Home:{screen: HomePageRoute,
-    navigationOptions:{
-      tabBarLable: 'Home',
-      tabBarIcon:({tintColor})=>(
-        <Icon name="home" color={tintColor} size={24}/>
-      )
+    /*Route configurations*/
+    Home:{screen: HomePageRoute,
+      navigationOptions:{
+        tabBarLable: 'Home',
+        tabBarIcon:({tintColor})=>(
+          <Icon name="home" color={tintColor} size={24}/>
+        )
+      }
+    },
+    Appointments:{screen: AppointmentsPageRoute,
+      navigationOptions:{
+        tabBarLable: 'Appointments',
+        tabBarIcon:({tintColor})=>(
+          <Icon name="calendar" color={tintColor} size={24}/>
+        )
+      }
+    },
+    Todo:{screen: TodoPageRoute,
+      navigationOptions:{
+        tabBarLable: 'Todo',
+        tabBarIcon:({tintColor})=>(
+          <Icon name="list" color={tintColor} size={24}/>
+        )
+      }
     }
-  },
-  Appointments:{screen: AppointmentsPageRoute,
-    navigationOptions:{
-      tabBarLable: 'Appointments',
-      tabBarIcon:({tintColor})=>(
-        <Icon name="calendar" color={tintColor} size={24}/>
-      )
-    }
-  },
-  Todo:{screen: TodoPageRoute,
-    navigationOptions:{
-      tabBarLable: 'Todo',
-      tabBarIcon:({tintColor})=>(
-        <Icon name="list" color={tintColor} size={24}/>
-      )
-    }
-  }
-},{
-  initialRouteName:'Home',
-  //shifting: true,
-  activeTintColor: '#263238',
-  inactiveColor: '#b0bec5',
-  barStyle: { backgroundColor: '#eceff1' },
+  },{
+    /*Navigator configurations*/
+    initialRouteName:'Home',
+    activeTintColor: '#263238',
+    inactiveColor: '#b0bec5',
+    barStyle: { backgroundColor: '#eceff1' },
 })
