72e51d1d3ee7465e5c3f2fd5ad6921093c62c6e8
Miriam Lillebo <miriamsl@stud.ntnu.no>
2018-10-17 17:30:11 +0200

    Ryddet og kommentert i kode, samt fikset en test for TodoPage #23 #26

--- pro3/__tests__/TodoPage-test.js
+++ pro3/__tests__/TodoPage-test.js
@@ -3,9 +3,11 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TodoPage from '../src/components/Todo/TodoPage';
 
+const navigationEx = { addListener: jest.fn() };
+
 it('renders correctly', () => {
 
-const tree = renderer.create(<TodoPage/>).toJSON();
+const tree = renderer.create(<TodoPage navigation={navigationEx}/>).toJSON();
 expect(tree).toMatchSnapshot();
 spyOn(console, 'error');
 });
