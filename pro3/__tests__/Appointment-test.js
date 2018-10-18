de04c753ae6e9e6bd5caf1be58334e262b524fa2
Heidibr <heidilb@stud.ntnu.no>
2018-10-18 14:44:20 +0200

    Tester storeItem og decreaseScore og increaseScore

--- pro3/__tests__/Appointment-test.js
+++ pro3/__tests__/Appointment-test.js
@@ -6,6 +6,9 @@ import MockAsyncStorage from 'mock-async-storage';
 import { AsyncStorage as storage } from 'react-native'
 
 const navigationEx = { addListener: jest.fn() };
+let scoreEx = 0;
+
+const appoints= renderer.create(<Appointment navigation={navigationEx}/>);
 
 //Implements mocks
 const mock = () => {
@@ -16,8 +19,7 @@ const mock = () => {
 //Sjekker at komponenten rendrer riktig, og kjører en snapshot test.
 it('should render corrrectly', async () => {
     mock();
-    const tree = renderer.create(<Appointment navigation={navigationEx}/>).toJSON();
-    expect (tree).toMatchSnapshot();
+    expect (appoints).toMatchSnapshot();
     jest.unmock('AsyncStorage');
 });
 
@@ -28,13 +30,39 @@ it('Mock Async Storage working', async () => {
     expect(value).toBe('12')
 });
 
-it('setState is called when getAppScoreAsync runs', async () => {
-    const component = renderer.create(<Appointment navigation={navigationEx} score = {score}/>)
-    await storage.setItem('Key', '12')
-    let score = await storage.getItem("Key");
-    const instance = component.root.instance;
-    const mockSetState = jest.fn();
-    instance.setState = mockSetState;
-    instance.getAppScoreAsync();
-    expect(mockSetState).toHaveBeenCalled();
+describe('score', () => {
+    test('setState is called when increaseScore', async () => {
+        const instance = appoints.root.instance;
+        const mockSetState = jest.fn();
+        instance.setState = mockSetState;
+        instance.increaseScore();
+        expect(mockSetState).toHaveBeenCalled();
+    });
+
+    test('should change state when updateTodo is called', () => {
+      const instance = appoints.root.instance;
+      const mockSetState = jest.fn();
+      instance.setState = mockSetState;
+      instance.decreaseScore(2)
+      expect(mockSetState).toHaveBeenCalled();
+      })
 });
+
+describe('storage', () => {
+    test('storeItem should set state with no errors', () => {
+        const instance = appoints.root.instance;
+        instance.storeItem({}).then((error) => {
+            expect(error).toEqual(null);
+            expect(instance.setState({})).toBeCalled();
+        });
+    });
+
+    test('storeItem should store in AsyncStorage with no errors', () => {
+        const instance = appoints.root.instance;
+        instance.storeItem({}).then((error) => {
+            expect(error).toEqual(null);
+            expect(storage.setItem).toBeCalledWith('test', JSON.stringify({}));
+        });
+    });
+});
+
