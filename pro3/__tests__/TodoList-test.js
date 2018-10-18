84e514e1df36e686bacb2cf1768cbebb94121dfe
Miriam Lillebo <miriamsl@stud.ntnu.no>
2018-10-18 10:12:42 +0200

    Ryddet litt i appointments, samt lagt til test for homepage #23 #26

--- pro3/__tests__/TodoList-test.js
+++ pro3/__tests__/TodoList-test.js
@@ -3,8 +3,12 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TodoList from '../src/components/Todo/TodoList';
 
+const tasksEx = [{"key":"0","text":"Hei"}, {"key":"1","text":"Hallo"}]
+
 it('renders correctly', () => {
-const tree = renderer.create(<TodoList/>).toJSON();
+const tree = renderer.create(<TodoList tasks={tasksEx}/>).toJSON();
 expect(tree).toMatchSnapshot();
 spyOn(console, 'error');
 });
+
+
--- pro3/src/components/Homepage/Homepage.js
+++ pro3/src/components/Homepage/Homepage.js
@@ -15,7 +15,7 @@ class Homepage extends React.Component {
 
         this.props.navigation.addListener("didFocus", () => {this.retrieveTaskScoreAsync()})
         this.props.navigation.addListener("didFocus", () => {this.retrieveAppScoreAsync()})
-        console.log('porps i const', this.props)
+        //console.log('porps i const', this.props)
     }
 
 
